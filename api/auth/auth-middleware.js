6fe0bb67e10f664103a739281119dbeb38b0094b
aleksandr-ardanov <baraboo312@gmail.com>
2021-04-20 14:32:13 -0500

    model done, middleware in process

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,21 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
 
 const restricted = (req, res, next) => {
+   const token = req.headers.authorization
+   if (!token){
+     res.status(401).json({"message": "Token required"})
+   } else {
+     jwt.verify(token,JWT_SECRET,(err,decoded) => {
+       if (err){
+         res.status(401).json({"message": "Token invalid"})
+       }
+       else{
+         req.decodedToken = decoded
+       }
+     })
+   }
+  
   /*
     If the user does not provide a token in the Authorization header:
     status 401
